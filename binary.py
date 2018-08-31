5278bc00b9237f8e429263d2b2239418f8adb6bc
Vijay Anand <avijay204@gmail.com>
2018-09-01 01:37:35 +0530

    Add decimal to binary

---
+++ binary.py
@@ -0,0 +1,24 @@
+import math
+import os
+import random
+import re
+import sys
+if __name__ == '__main__':
+  n = int(input())
+  r=[]
+  while n>0:
+  	r.append(n%2)
+  	n=n//2
+  i=len(r)  
+  while i>0:
+   print (r[i-1])
+   i -= 1
+
+
+
+
+
+
+
+
+
